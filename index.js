92d0370332c46b548072c3a1039340098d525aca
Gregory Frantzen <gfrantze@poop.local>
2016-04-02 22:24:39 -0700

    s io

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg.TEXT);
+    socket.broadcast.emit('chat message', "this is a test");
   });
 });
 
